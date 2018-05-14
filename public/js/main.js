73695500ab277f81b5c23e06dc713c14f15facf1
Alper Silistre <alpersilistre@gmail.com>
2018-05-14 10:19:43 +0300

    Express and Node added to the project

---
+++ public/js/main.js
@@ -0,0 +1,20 @@
+$( document ).ready(function() {
+  $("#sendForm").click(function() {
+    let name = $("#name").val();
+    let mail = $("#mail").val();
+    let password = $("#password").val();
+    let under13 = $("#under_13").val();
+    let over13 = $("#over_13").val();
+    
+    let bio = $("#bio").val();
+
+    let job = $("#job").val();
+
+    let development = $("#development").val();
+    let design = $("#design").val();
+    let business = $("#business").val();
+
+    let button = $("#sendForm");
+
+  });
+});
